49451e6574fb5395cffc3bb9d001f42eebac35af
harshj888 <64105502+harshj888@users.noreply.github.com>
2020-10-08 10:36:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
-
+import numpy as np 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    arr = np.array( [[ 1, 2, 3], 
+                 [ 4, 2, 5]] ) 
+    return 'Hello World!',arr.size
 
 
 if __name__ == '__main__':
